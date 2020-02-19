29cc69e79042f8714a4ceb362708f1bb1c25c070
James Bell <jamesleonbell@gmail.com>
2020-02-18 16:53:16 -0800

    changes

--- porttest.py
+++ porttest.py
@@ -2,9 +2,20 @@ import serial.tools.list_ports
 import serial
 
 a=serial.tools.list_ports.comports()
+
+ArduinoMegaPort = 'None'
+ArduinoNanoPort = 'None'
+ArduinoNano2Port = 'None'
+
+# comment
 for w in a:
     print("Port:", w.device,"\tSerial#:", w.serial_number)
     if w.serial_number == '558383437333512132D0':
         ArduinoMegaPort = w.device
 
-print('Arduino Mega Port', ArduinoMegaPort)
+    if w.serial_number == '5':
+        ArduinoNanoPort = w.device
+
+print('Arduino Mega Port: ', ArduinoMegaPort)
+print('Arduino Nano Port: ', ArduinoNanoPort)
+print('Arduino Nano2 Port: ', ArduinoNano2Port)
