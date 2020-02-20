c7247ac131961b723eda7f327f21299e3b506f25
James Bell <jamesleonbell@gmail.com>
2020-02-19 17:17:32 -0800

    changes

--- shared/control.py
+++ shared/control.py
@@ -15,7 +15,8 @@ try:
     time.sleep(1)
     print('Light strip connection: Successful')
 except NameError:
-    arduino2 = serial.Serial('/dev/ttyUSB1', 9600)
+    arduino2 = serial.Serial('/dev/ttyUSB0', 9600)
+    time.sleep(1)
 except:
     useArduino2 = False
     print('Light strip connection: Unsuccessful')
